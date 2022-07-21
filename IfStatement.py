d56b0fb175e73da6750fce29d422ec6734330e24
Falak-dev <109524913+Falak-dev@users.noreply.github.com>
2022-07-21 19:04:26 +0530

    Add files via upload

---
+++ IfStatement.py
@@ -0,0 +1,10 @@
+day_of_week = input("what day of the week is it today? ")
+
+if day_of_week == "Monday":
+    print("Have a great start to your week!")
+elif day_of_week == "Tuesday":
+    print("It's Tuesday.") 
+else:
+    print("Full speed ahead!")
+
+print("This always runs.")
